7cde8b9204e5dfa112e836c10f232ef3c081a531
carreuky <carreuky@gmail.com>
2023-01-13 09:58:24 +0300

    mapped card data insert

--- src/component/RadioCard.jsx
+++ src/component/RadioCard.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 
-export default function RadioCard() {
+export default function RadioCard({chan,setchanNow}) {
   return (
     <div class="cursor-pointer flex flex-col items-center mx-auto p-2"
+    onClick={()=>setchanNow(chan)}
     >
       <img
         class="object-cover w-36 h-32 my-2  shadow"
-        src=''
+        src={
+            chan.favicon === ""
+              ? "https://media.istockphoto.com/id/1139509180/photo/transistor-radio-receiver-on-wood-table-in-home-interior-3d.jpg?s=612x612&w=0&k=20&c=kB0lTO6XgUUqz23xrauFdB1xNnKiwADX31AWKabSeYg="
+              : chan.favicon
+          }
+        alt={chan.name}
       />
       <div class="flex flex-col items-center">
-        <p class="text">Cool</p>
-        <p class="text-sm uppercase">Kenya</p>
+        <p class="text">{chan.name}</p>
+        <p class="text-sm uppercase">{chan.country}</p>
       </div>
     </div>
   );
